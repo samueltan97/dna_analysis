4b0677455f55c29709ee040abcbc14240c5ee0ac
Samuel <samueltan1897@gmail.com>
2020-02-03 08:06:10 -0500

    Stuck debug

--- RecombinationPoint.py
+++ RecombinationPoint.py
@@ -10,16 +10,19 @@ class PermutationCombination:
         self.size += 1 if end else 0
         pointer = self.dict
         # TODO: change key method since None cannot equals to something
+        for index, key in enumerate(keys):
+            pointer = pointer[key]
+        pointer[permutation] = dict()
+        # print(self.dict)
+    def remove_permutation(self, keys: [], permutation):
+        pointer = self.dict
         for key in keys:
             pointer = pointer[key]
-        pointer = {str(permutation): None}
-        if self.size == 5:
-            return (self.dict, True)
-        else:
-            return (self.dict, False)
+        # print("pre", self.dict)
+        del pointer[permutation]
+        # print("post", self.dict)
 
-    def remove_permutation(self, keys: []):
-        pass
+
 
 class RecombinationAnalysis:
 
@@ -55,10 +58,18 @@ class RecombinationAnalysis:
 
     def find_recombination_point(self):
         self.recursive_recombination_check(self.parent1, self.parent1_data, self.parent2, self.parent2_data, self.child, self.child_data, [])
+        print(self.analysis_result.dict)
         return self.analysis_result.dict
 
     def recursive_recombination_check(self, parent1, parent1_data, parent2, parent2_data, child, child_data, keys: []):
         if self.analysis_result.size < 5:
+            print("child", len(child))
+            print("child", child[:3])
+            print(keys)
+            # print("parent1", len(parent1))
+            # print("parent2", len(parent2))
+            # print("parent2", parent2[:3])
+            # print(len("GATGTCGCCTTCGAGTCCTATAGTGCGTCGCTGGTCCGCAATATTCGTCGCATCTCCATGAAGGAGGGGTGGATAGTCATCGGTCGAGATAGTAACGTAATAGTGCCTCTGACGAGCCCATGCCTAAGCGACAACCCTTGACAAAGCGCGACCTTAGGCGGACACCCCGTCCAGGCTGACTGAGGGGCAACTTTAAAAACCTACGGTACAGGCTCCTACTACGTACATACGTTTATACCCAGGTCCAGTCTTAGCGCGGTACCAGTTAATACTACACCAGTGGTCCACGCACACACTATCGAGGCGAATGTTGCCGTTAACGAAGAACCTCCCCGGAATCTGAGACCCTGTGAATGGTAAGTTAGTCCGCCAACCTCAATTATCCTTAGCTAAACACCTCGACGTAGCTCATATCACCCAGTACACTCGTAACGACTAAAAGTGTAGAGTCAAAGCAGTCCACTGTGAGGGGCAACTGGTCGTACTCTCCGGGGGAGACTTGTGTAATTAGGTGTTCGGAGGTCCCATAGTCGTCCCCTAAAGCAAGACCTCTCGCCAGGAATGTCGACGAGTTGAAGACCTGGACTTGCTACACCCGAGATAGACTAGGATGGTACTGGAGCGTAGCAGCTCATTATTGACTGCATTCAAATGCTCCGTTGGGCCTTATTTACTCGGAAAATTTACCGTGAACACTGCTAAGACTGCTCGATTCGTATTAAGGAAACATACTTTGGGTCTGAAACTATGAAACTGCACCCAGCGCCTAAAGTAGTTCAGCTAGCCTGCGCGCGGGGGGAATAGTAACTGCGTTAGCACTCTTGCAACCAGTCAACCCCTGCCCAAAGAGTGGGCTTGAGGGGGAGATCATCTTTTCAAACAATAGTTTAATTGTACTAGATCCACGACGAAGCGAAGATCGTCGATGCCTTGTTGTAACTATGACTGCTCATGCTCCGTTAAAGCCCAACGGTGTGAGGCCACCTGGCATCCCGCATGGACAATTCCTCGAGGTACAGCTCCCCATTCTCGAAATGGGCGCCGGCATGTTTACACAGGTACGTTACACTCCCTACAGTTGGCGCGTCTGGCCACAGCTAAGCCGTGGAAAAACTGTTAGCCTACGACAGTGTTGTCGCTTCTACTCGACTGGGGGCGACTGGTACCTTTAAAAATAAAGTTAGAGCAGGGGATCATCATCGATCTCGTACCTGTTGCCACAGCCTGGGCCATACCCTTAAGTGCTAGTTCCGATCTCTCAACTGGGCAGCGTGATAGACCTATTTAAATGCTAACGGCTGTGTACAAGCAGTCCAAATTGGGCCTCCTTCTTCTTGCGAGTACTAAACCCTTGACACTTTCGTAATTCACCAAACTGAACCGGGTATAATTTTCGAATAGCCGTCGCTTCTCCGGAGGACGTCCTCTTACCTCGGAGTGGGGTTTCGGCCGGACCCACGTCCATGGCCTGCTCCAAAGTTAGTTAAATCTACTTCTATTAGCAATATCCTTTATACCACAAGGGCCGAGGCTATGATGCACCACTGCAGATCACCTGAGTGATTTAAGTAATGAGGGGCAGCACGTAACGCGGCGTTATATTAGCTTCTAGAACTCCCACTCTGTGGAACGAATCTGAAATACGTCTATAGGTTGATAGCGTTCGCTAAATTTGGTCCGTACCGTACTGGGATAGTAACTAGAACATCTACCCACTATGGGAAAGGGAAGGAGCATTGGACTCCAAACTAAGGACTTTACTCACGGGATCGCGCAAGCCCAGACCAGTAGTCACCGGCTATGTCAGAGGAGACAAAAATGTCAAGCGTATTTGTTATGGTGAGACGACACAGATAGTCAAGACGGTCCAAATGTTCACCGTCGACGGACCCGATCTAATGTGAGTTACCCTTGGTGAGTGGTTCCCCGTGCGGATCGCCCTTACGCGCTGGTGAAGGGTCATCGACCCAGGAACACGGCTGAATGTAATCTTTCCCAACGCTGGCGTTCGATTTATCAGTGACAGCAGCTACAGATCCAGACGAGAAGAAGGCGATTAGAGCGTGCACGTTAGATGTTACTGACACTTCAGACGTGCACGAAGCCCCGCCTCCACGTAAGGGGTATGGGTCGAACGAGTGTCATGAGCTAACTACGAACCCTACTTTTATGGGGAAGGACCTATGTATATCCCTAAATTCAACGTCCTATGATTTGGGCACGACTATTCTCCAAACCAAAGCTTAGGCAAGGTAGCATGATACCTAACATGAACCGACTTCCCACAACGAATTTTATTATCACGTATTACTAAAGTAGAAGAGTACCTCGGCTGCGCAGATAAATTCATAACATGTCCCATTACGCCCCACGCCGGCTGCGCTAAGGAGACTTTGGATCTCCTTGCAGTGCAACATAATCGCACGCCGGTCTACACATCCGTATTTTACGGTCACCAACTCAAGATGGCTATGCACGCTATCGCAAAATCACCCCCCATTTCAAGAACGATCACAGACCCAGACATGAATCTCGTCAACATCACCAGGCCACAAGGTATTTTTCTATAAAATACTGCAGCTGAGGATAGCCGACCACGCCGGGGCAGCCAGGCCAGATTCTACTCCCATGCGACCGAACTCTATGACGGAGCGGAACCTTGTGACTTCGCAGTTTGGGGCCTATGCTCGTAGGGGTGCGAGAGCCCGAATTGGCATCTCGCGGGCCTGGGCATGCCTTTATATCCAGCCAGGTTCCGTATACGCGAACGGCGTAGTGTGCTGTCGGTGATGTCCCCGGTTCACGCGTGCGGGGATTGAGTCTGTGTGGTAGTTATTAGGCATGACACGAGCGCATATCTGAATGCCCCGGGGCCCGACCACAAATTTCTGGCTAGGCGTCCGTGCAGATGTGCGCTATTCCGTTACATAGGTACACGAGGGCTTCTGTCCATTACAAGCCCCAGAGAGTTTCCAGGCGACTCCATAATGGCATTATGGAAAGTGGGGAGCTGTTGCCGCCTCTTCTTGAAAGATTAAGGACGTCTTATGGCCGTGACATAGCAGGAAACCCCCTATCTGGCTACTACCTAGTTTCTCCTGGGTCTGAGGGGCGAGAACCGGCGGTCCTTATATTGAAACTCGGATGGTGAGGTTGCTGCTCCTACTAACATAAGCCCGATCAAAGAGTTCAGACAGGCCGTGTCGATCGACACAACAACAATCCACGTCCCGAGGGGGCGGTGCCGTACCTGGCGGCTGAATCGCTGCTGCACATTCCCCAGCTTCTAGCCGTGCGTCTCGGCTAAATTATTCCCTTTGATTCTCTCGCGCGGTCGGTTCGGACGTAGTGGAGTAGATGTTACCTAAAACGTACGAAGGCGCCGGTACTCCACGACTTGCTCTGATATCGCCCCGTCTTAACAATCAACCGCTGTTGTGAACTATAATCTACAGAAGTCACGCCACAGCGTAGGCCTGCTCATTGCGCAATGGAGGTGTGGCGTTGAGTGCTGAGGGAGGTGTTAACAAACAGGACGGGTCATTTGGATGCGAATTCCCCTCCCGCGCATGGTCCGATAAGACTAAGATGTTAACCAAGCAGGGAGATGATACTAGTTCAACCGTCAGAGACACATCTAACCAAGTAGACGAAAGGCGTGGCAGCAGCTAACCATCGAGGTAGTGAGACGTCAAAGGGTGTTAGACTTTATCAGCCAAAAGTAATATTTCTTGCGTATGAGCTTACCCTTCATGTAGGACGTAAGCGTCCAACCTTGCTACACGTAGCTGGTGCTATTTCGAGCTGCATTACCCTTCCCATTTCTTAGTAGCTGGGAAGGTTACCGTAGCCGTCACTACCTCTTACTAAGCCTAAAAGATCTTCGCGCCGTTGTAAGTTTAACCAGGAGAACCCCGCGGACTAAGTAGACGGCGGCTAGCCAATTATTTATTGCAGTGCCTTATTGGTGTCCACGTTTTCGACCTTCTAGCCGGAGTGCACTTGTTCGACTTCATCACTACCTGCAGCACTTTGAACATTGTGGACCTTCAGTCCGAGTTGTCATATTAACTGATTCGATTCTCTTACCTAGATATACTCCCAGTAACCACTTCCCCCGCCACTCTGCGGCTTATGGACAGTCAGGGCAGGACGTTCATTCGTTAATATCACTCGCCTGGTCTCATCAGTATCACAGCGAATGAGTACAGCTCCCGGGGCATCACCGACATTTATAACCCCGGTTTTTTCCAGGCCCTCTTGTAAAATTCGAACAGTGGACGCCATACCACCGGTGCAAACGGACTAATTCGTAGGACAAACATGTGGAGACAATGTTGGCTCGCAGCTAGGTGTACCCAGATAGGTAACAGCATCGAAGTATGTGACGCTCTGCGCTCTCGCCATTCCCGAATACAACATCCAGTTGTCATATCCCCCTAAAAACTCGACGGGTCAGCATAAAGGTTCTTGGGTGCGATGGGGTCCGCAGGAATTGCAAGGAGATCTTGTGCCAGGCACGCGCTGACAAGATAAACCAGGGAGATAAGGATGAGCGATTCAGCTCTAGAAGGGAAGGAACGCCGAGCGAGTAAGCCAGTGCCCCGTTTTCTGCCACGCGCCCAATTATCTTTCCAGGACAAAGAGGTCGGTGCAACTGGGGGTAGAGACTTGCCAATTCTGTATCACACATGCAGCGCAAGATTCCGGTACCTAACCCCAGGTAAGTCGATTGAGCCCCGAGGGCGGGTGTCATTGACAGTAATCGTACTGTTGGCACGCCGAACGCTCGCGCGGACTTACTGTAAATGGTGTTTGCGATACCGGGGAATGTCTTCATCACTCCTAAGACTGCGATATCGTGCCAACTGATGTCGATTTTAATATGCACTCGTTGATCTGAACGGCGGCCGCGTTCTGTTGCAGGAGTGCATGGTCGTGTAGGTGAGCACGTACTCATAATGAATGGGCAGCCATCTTGTACGAAAAGATTCACACGTACCGGCGACTGTATATCGCATATAACTTATTATCCCGGATGCGGTAGCCCCGCTGACGGCCAATAGAGGTACCACTCATTCATGTGGCTGATTCCACTT"))
             d_child = child[:]
             d_child_data = copy.deepcopy(child_data)
             d_parent1 = parent1[:]
@@ -67,6 +78,7 @@ class RecombinationAnalysis:
             d_parent2_data = copy.deepcopy(parent2_data)
             first_child_char = d_child[0]
             match_dataset = self.compare_xdiff(first_child_char, d_parent1_data, d_parent2_data, d_child_data)
+            print(match_dataset)
             if len(match_dataset) == 0:
                 return False
             check_queue = queue.LifoQueue()
@@ -77,81 +89,83 @@ class RecombinationAnalysis:
                 target_parent = d_parent1 if match_data[1] == 1 else d_parent2
                 target_parent_data = d_parent1_data if match_data[1] == 1 else d_parent2_data
                 counter = 0
-                while target_parent[0][match_data[0][1] + counter] == d_child[counter]:
+                while target_parent[match_data[0][1] + counter] == d_child[counter]:
                     parent_index = target_parent_data[target_parent[match_data[0][1] + counter]].index(match_data[0][1] + counter)
-                    target_parent_data[target_parent[match_data[0][2] + 1 + counter]].pop(parent_index)
+                    target_parent_data[target_parent[match_data[0][1] + counter]].pop(parent_index)
                     d_child_data[d_child[counter]].pop(0)
                     counter += 1
 
-                recombination_point = (match_data[0][2] + counter, match_data[1])
+                recombination_point = (match_data[0][1] + counter, match_data[1])
                 if match_data[1] == 1:
-                    d_parent1 = d_parent1[:match_data[0][1]] + d_parent1[(match_data[0][1] + counter + 1):]
+                    d_parent1 = d_parent1[:match_data[0][1]] + d_parent1[(match_data[0][1] + counter):]
                     d_parent1_data = self.build_processed_chromosome_data(d_parent1)
                 else:
-                    d_parent2 = d_parent2[:match_data[0][1]] + d_parent2[(match_data[0][1] + counter + 1):]
+                    d_parent2 = d_parent2[:match_data[0][1]] + d_parent2[(match_data[0][1] + counter):]
                     d_parent2_data = self.build_processed_chromosome_data(d_parent2)
-                d_child = d_child[counter + 1:]
+                d_child = d_child[counter:]
                 d_child_data = self.build_processed_chromosome_data(d_child)
 
                 if len(d_child) == 0:
-                    self.analysis_result.add_permutation(keys + [str(recombination_point)], recombination_point, end=True)
+                    self.analysis_result.add_permutation(keys, recombination_point, end=True)
                     return True
                 else:
-                    self.analysis_result.add_permutation(keys + [str(recombination_point)], recombination_point)
-                    if self.recursive_recombination_check(d_parent1, d_parent1_data, d_parent2, d_parent2_data, d_child, d_child_data):
+                    self.analysis_result.add_permutation(keys, recombination_point)
+                    if self.recursive_recombination_check(d_parent1, d_parent1_data, d_parent2, d_parent2_data, d_child, d_child_data, keys + [recombination_point]):
                         return True
                     else:
-                        self.analysis_result.remove_permutation(keys + [str(recombination_point)])
+                        self.analysis_result.remove_permutation(keys, recombination_point)
                         return False
 
-    def compare_xdiff(self, char):
-        parent1_xdiff = self.parent1_data[char+"Diff"]
-        parent2_xdiff = self.parent2_data[char+"Diff"]
-        child_xdiff = self.child_data[char+"Diff"]
+    def compare_xdiff(self, char, parent1_data, parent2_data, child_data):
+        parent1_xdiff = parent1_data[char+"Diff"]
+        parent2_xdiff = parent2_data[char+"Diff"]
+        child_xdiff = child_data[char+"Diff"]
         parent1_ranges = []
         parent2_ranges = []
         child_counter = 0
+        parent_counter = 0
         current_range = []
-
         for i in range(len(parent1_xdiff)):
-            if parent1_xdiff[i] == child_xdiff[child_counter] and len(current_range) == 0:
-                current_range = [1, self.parent1_data[char][i], i]
-                child_counter += 1
-            elif parent1_xdiff[i] == child_xdiff[child_counter] and len(current_range) > 0:
-                current_range[2] = self.parent1_data[char][i + 1]
-                current_range[0] = i - current_range[1]
-                child_counter += 1
-            else:
-                if len(current_range) > 0 and current_range[0] > 1:
-                    parent1_ranges.append((current_range, 1))
-                current_range = []
-                child_counter = 0
-
+            while i + parent_counter < len(parent1_xdiff) and child_counter < len(child_xdiff) and parent1_xdiff[i + parent_counter] == child_xdiff[child_counter]:
+                if len(current_range) == 0:
+                    current_range.append(1)
+                    current_range.append(self.parent1_data[char][i])
+                    current_range.append(self.parent1_data[char][i])
+                    child_counter += 1
+                    parent_counter += 1
+                else:
+                    current_range[2] = self.parent1_data[char][i + 1]
+                    current_range[0] = current_range[2] - current_range[1]
+                    child_counter += 1
+                    parent_counter += 1
+            if len(current_range) > 0 and current_range[0] > 1:
+                parent1_ranges.append((current_range, 1))
+            current_range = []
+            child_counter = 0
+            parent_counter = 0
+        print("Parent1_ranges", parent1_ranges)
         current_range = []
         child_counter = 0
+        parent_counter = 0
 
         for i in range(len(parent2_xdiff)):
             if parent2_xdiff[i] == child_xdiff[child_counter] and len(current_range) == 0:
-                current_range = [1, self.parent2_data[char][i], i]
+                current_range = [1, self.parent2_data[char][i], self.parent2_data[char][i]]
                 child_counter += 1
             elif parent2_xdiff[i] == child_xdiff[child_counter] and len(current_range) > 0:
                 current_range[2] = self.parent2_data[char][i + 1]
-                current_range[0] = i - current_range[1]
+                current_range[0] = current_range[2] - current_range[1]
                 child_counter += 1
             else:
                 if len(current_range) > 0 and current_range[0] > 1:
                     parent2_ranges.append((current_range, 2))
                 current_range = []
                 child_counter = 0
-
         parent_ranges = parent1_ranges + parent2_ranges
         parent_ranges.sort(key=lambda x: x[0][0])
-        return parent_ranges
+        return parent_ranges[-5:]
 
 
 
 if __name__ == "__main__":
-    RecombinationAnalysis("data.txt")
-    data = read_data_as_chromosomes("data.txt")
-    mosaic_relationship = find_mosaic_relationship()
-    find_recombination_point(data, mosaic_relationship)
+    RecombinationAnalysis("data.txt").find_recombination_point()
